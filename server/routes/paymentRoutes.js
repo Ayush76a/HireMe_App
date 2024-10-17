8bcb1435ad06a1e4a600c7e5527d6f57323d538d
Ashish Kumar <ak503005@gmail.com>
2024-10-18 00:39:44 +0530

    Update paymentRoutes.js

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 // auth Middleware
 const protect = require("../middleWare/authMiddleware");
-
+// hello ji
 const {
   Pay,
   Verify
